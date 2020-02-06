15ce6c1aa18931f4d9553ab7ea57864d83a49ece
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2020-02-06 14:01:01 +0100

    commencer le header
    j'ai commencer le header les mec Gautier a toi de jouer si tu veux ;)

--- application/views/header.php
+++ application/views/header.php
@@ -1,3 +1,43 @@
 <header>
+<div class="container">
+<div class="col-12">
+<div class="row">
+    <div class="container">
+    <img class="" src="<?= base_url('assets/img/').'logo village green.png'?>" alt="logo de l'entreprise village green" title="logo">
+    </div>
 
+    <div class="container">
+    <div class="row">
+    <ul>
+        <li><a href=""></a>infos</li>
+        <li><a href=""></a>Espace Client</li>
+        <li><a href=""><img src="<?= base_url('assets/img/').'picto panier.png'?>" alt=""></a></li>
+        <li><img src="<?= base_url('assets/img/').'picto pays.png'?>" alt=""></li><!--menue bootstap lang-->
+    </ul>
+    </div>
+    <div class="row">
+    <ul>
+        <li><a href=""></a>Produits</li>
+        <li><a href="">Service</a></li>
+        <li><a href="">Aide</a></li>
+        <li><a href="">A propos</a></li>
+    </ul>
+    </div>
+     
+    <div class="row">
+    <ul>
+        <li><a href="">Guit/Bass</a></li>
+        <li><a href="">Batteries</a></li>
+        <li><a href="">Clavier</a></li>
+        <li><a href="">Studio</a></li>
+        <li><a href="">Eclairage</a></li>
+        <li><a href="">DJ</a></li>
+        <li><a href="">Cases</a></li>
+        <li><a href="">Accessoires</a></li>
+    </ul>
+    </div>
+    </div>
+</div>
+</div>
+</div>
 </header>
