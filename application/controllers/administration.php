838950eec3668ad306d7beef09d77b180aa359c7
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2020-02-06 14:24:42 +0100

    configuration

---
+++ application/controllers/administration.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Administration extends CI_Controller
+{
+  
+}
---
+++ application/controllers/commercial.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Commercial extends CI_Controller
+{
+  
+}
