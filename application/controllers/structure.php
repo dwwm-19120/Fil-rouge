88cea27340fd267e99970c4757579112623ae55e
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2020-02-06 14:36:15 +0100

    Update structure.php

--- application/controllers/structure.php
+++ application/controllers/structure.php
@@ -3,5 +3,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Structure extends CI_Controller
 {
-  
+    /*
+    --------------------------------------ACCEUIL------------------------------------------------
+    */
+  public function acceuil ()
+  {
+      $this->template->display('acceuil');
+  }
 }
