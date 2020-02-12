4c77d873dd205d4b7ea75605079bf8b2c8788f36
gauth80 <gaboukraa@outlook.fr>
2020-02-12 15:06:50 +0100

    header2

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,11 +1,12 @@
-<footer>
-<br>
-<div class="container footer"> 
-<h4>recevez nos offres exclusives</h4>
+    <footer>
+        <br>
+        <div class="container footer"> 
+            <h4>recevez nos offres exclusives</h4>
+        </div>
+        <div class="container">
+            <h4>suivez-nous par ici!</h4>
+            <a href="https://fr-fr.facebook.com"><img src="<?= base_url('assets/img/').'picto facebook.png'?>" alt="Nous suivre sur Facebook" title="Nous suivre sur Facebook"></a>
+            <a href="https://twitter.com/?lang=fr"><img src="<?= base_url('assets/img/').'picto twitter.png'?>" alt="Nous suivre sur Twitter" title="Nous suivre sur Twitter"></a>
+        </div>
+    </footer>
 </div>
-<div class="container">
-<h4>suivez-nous par ici!</h4>
-<a href="www.facebook.com"><img src="<?= base_url('assets/img/').'picto facebook.png'?>" alt=""></a>
-<a href="www.twiter.fr"><img src="<?= base_url('assets/img/').'picto twitter.png'?>" alt=""></a>
-</div>
-</footer>
