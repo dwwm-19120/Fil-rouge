9c432aa9110543405146ba3317e8cddd957a843d
Nicolas <56954217+nicofoviaux@users.noreply.github.com>
2020-02-18 16:30:57 +0100

    Update produit.php

--- application/models/produit.php
+++ application/models/produit.php
@@ -1,5 +1,21 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 class Produits extends CI_model
 {
-    
+   /**
+    *\brief affichage produit
+    * \return ltableau de variable
+    * \author FOVIAUX Nicolas
+    * \date 17/02/2020
+    */
+    public function list()
+    {
+       $products= $this->db->get('produits');
+       return $products->row();
+    }
+
+
+
+
+
+
 }
