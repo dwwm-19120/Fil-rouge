c8dad5aff462add9528cf1b3273b2ae477466006
gauth80 <gaboukraa@outlook.fr>
2020-02-20 10:19:17 +0100

    projet

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,12 +1,132 @@
-    <footer>
-        <br>
-        <div class="container footer"> 
-            <h4>recevez nos offres exclusives</h4>
-        </div>
-        <div class="container">
-            <h4>suivez-nous par ici!</h4>
-            <a href="https://fr-fr.facebook.com"><img src="<?= base_url('assets/img/').'picto facebook.png'?>" alt="Nous suivre sur Facebook" title="Nous suivre sur Facebook"></a>
-            <a href="https://twitter.com/?lang=fr"><img src="<?= base_url('assets/img/').'picto twitter.png'?>" alt="Nous suivre sur Twitter" title="Nous suivre sur Twitter"></a>
-        </div>
+<footer role="footer" class="col-12 bg-dark">
+        <div class="row">
+            <form class="col-4 form-control" action="/" method="post">
+                <label for="foo-email">Recevez <strong>nos offres exclusives</strong></label>
+                <input type="text" name="foo-email" placeholder="Votre adresse e-mail">
+                <button type="submit" class="btn btn-success">Valider</button>
+            </form>
+            <figure class="col-4">
+                <figcaption><strong>Suivez-nous</strong> par ici !</figcaption>
+                <a href="https://fr-fr.facebook.com/" target="_blank" ref="nofollow"><img src="<?= base_url('assets/img/picto_facebook.png');?>" alt="Facebook" title="Réseau Facebook"></a>
+                <a href="https://twitter.com/login?lang=fr" target="_blank" ref="nofollow"><img src="<?= base_url('assets/img/picto_twitter.png');?>" alt="Twitter" title="Réseau Twitter"></a>
+            </figure>
+            <figure class="col-4">
+                <img src="<?= base_url('assets\img\chronopost.png');?>" alt="chronopost" title="eh bah nan">
+            </figure>
+            <article class="row">
+                <section class="col-3">
+                    <ul><h2>Contactez-nous !</h2>
+                        <li class="list-unstyled">Conseil / Commande téléphone</li>
+                        <li class="list-unstyled"><p>du lundi au vendredi de 8h à 19h,
+                        le samedi de 10h à 18h</p></li>
+
+                        <li class="list-unstyled"><p>Depuis la France:</p></li>
+                        <li class="list-unstyled"><strong class="numero">02 40 38 50 50</strong></li>
+
+                        <li class="list-unstyled">Belgique, Suisse, International :</li>
+                        <li class="list-unstyled"><strong class="numero">0033 2 40 38 50 50</strong></li>
+
+                        <li class="list-unstyled">Service après vente:</li>
+                        <li class="list-unstyled"><strong>02 51 80 68 76</strong></li>
+                        <li class="list-unstyled"><p>Contactez-nous par téléphone du lundi au samedi de 9h à 18h ou depuis votre compte client.</p></li>
+                        
+                        <li class="list-unstyled">Conseil pour choisir un instrument :</li>
+                        <li class="list-unstyled"><p>infovg[a]villagegreen.com</p></li>
+
+                        <li class="list-unstyled">Service Presse :</li>
+                        <li class="list-unstyled"><p>contact[a]villagegreen.com</p></li>
+
+                        <li class="list-unstyled">Village Green Recrute !</li>
+                    </ul>
+                </section>
+                <section class="col-3">
+                    <ul><h2>Village Green Stores</h2>
+                        <li class="list-unstyled">Ouverts de 10h à 19h30 non-stop.</li>
+                        <li class="list-unstyled"><p>Guitares - Amplificateurs - Effets:
+                        182 avenue Jean Jaurès
+                        <i>75019 Paris</i></p></li>
+
+                        <li class="list-unstyled"><p>Clavier - Home Studio - Sonorisation
+                        Equipement DJ - Eclairage:
+                        184 avenue Jean Jaurès
+                        <i>75019 Paris</i></p></li>
+
+                        <li class="list-unstyled"><p>Librairie Musicale:
+                        7 av. du Nouveau Conservatoire
+                        <i>75019 Paris</i></p></li>
+
+                        <li class="list-unstyled"><p>Instruments à vent:
+                        9/11 av.du Nouveau Conservatoire</p>
+                        <i>75019 Paris</i></li>
+
+                        <li class="list-unstyled"><p>Percutions:
+                        13/15 av.du Nouveau Conservatoire
+                        <i>75019 Paris</i></p></li>
+
+                        <li class="list-unstyled"><p>Métro Ligne 5, station Porte de Pantin.</p></li>
+
+                        <li class="list-unstyled"><p>Plan d'accèes / Visite virtuelle</p></li>
+                    </ul>
+                </section>
+                <section class="col-3">
+                    <ul><h2>Utiles !</h2>
+                        <li class="list-unstyled"><p>Qui sommes nous ?</p></li>
+                        <li class="list-unstyled"><p>F.A.Q.</p></li>
+                        <li class="list-unstyled"><p>Le blog Village Green</p></li>
+                        <li class="list-unstyled"><p>Vous avez un site Internet ?
+                        Devenez partenaire Village Green</p></li>
+                        <li class="list-unstyled"><p>Conditions générales de vente</p></li>
+                        <li class="list-unstyled"><p>Plan du site</p></li>
+                        <li class="list-unstyled"><p>Parrainage</p></li>
+                        <li class="list-unstyled"><p>Nouveautés</p></li>
+                        <li class="list-unstyled"><p>Assurance Woodbrass.com</p></li>
+                        <li class="list-unstyled"><p>Location d'instruments de musique</p></li>
+                    </ul>
+                </section>
+                <section class="col-3">
+                    <ul><h2>Village Green Preservation Society</h2>
+                        <li class="list-unstyled"><p>Toute l'actualité musical</p></li>
+                        <li class="list-unstyled"><p>Voir l'avis des musiciens</p></li>
+                        <li class="list-unstyled"><p>Concours: inscription et résultat... à vous de jouer !</p></li>
+                    </ul>
+                </section>
+            </article>
+            <div class="row">
+                <cite class="col-12">
+                    <p>Village Green est une entreprise 100% Made in France !</p>
+                    <p>Magasin de musique - Achat / Vente instruments de musique - Locationinstrumens de musique - Atelier de réparation - Vente accessoires de musique au meilleur prix.</p>
+                    <p>1989-2016 - RCS Paris B 222 333 444 Déclaration CNIL : 12345678.</p>
+                </cite>
+                <div class="col-12">
+                    <nav class="navbar navbar-expand-sm navfooter">
+                        <ul class="navbar-nav">
+                            <li class="list-unstyled"><a href="/">Guitares - </a></li>
+                            <li class="list-unstyled"><a href="/">Amplis / Effets - </a></li>
+                            <li class="list-unstyled"><a href="/">Claviers / Pianos - </a></li>
+                            <li class="list-unstyled"><a href="/">Synthés - </a></li>
+                            <li class="list-unstyled"><a href="/">Batteries / Percussions - </a></li>
+                            <li class="list-unstyled"><a href="/">Vents - </a></li>
+                            <li class="list-unstyled"><a href="/">Violons - </a></li>
+                            <li class="list-unstyled"><a href="/">Home Studio - </a></li>
+                            <li class="list-unstyled"><a href="/">Sonorisation - </a></li>
+                            <li class="list-unstyled"><a href="/">Deejay - </a></li>
+                            <li class="list-unstyled"><a href="/">Eclairage - </a></li>
+                            <li class="list-unstyled"><a href="/">Micros - </a></li>
+                            <li class="list-unstyled"><a href="/">Casque - </a></li>
+                            <li class="list-unstyled"><a href="/">Enregistreurs - </a></li>
+                            <li class="list-unstyled"><a href="/">Lifestyle - </a></li>
+                            <li class="list-unstyled"><a href="/">Destockage - </a></li>
+                            <li class="list-unstyled"><a href="/">Ecoles & Pros</a></li>
+                        </ul>
+                    </nav>                    
+                </div>
+                <div class="other">
+                    <p>Behringer - Bose - Conservatoire de musique de Paris - Fender - Focusrite Scarlett - Focusrite - Gibson 2016 - Gibson - Guitar Shop - Guitare Martin - Guitare Taylor - Guitare classique - Guitare folk - Guitare électrique IK Multimedia - Luthier Paris - Magasin DJ Paris - Magasin instruments de musique Paris - Magasin sonorisation Paris - Microphone USB - Musique - Namm - Partition alto - Partition clarinette - Partition flute - Partition guitare Partition harmonica - Partition musique - Partition piano - Partition saxophone - Partition trombone - Partition trompette - Partition vibraphone - Partition violon - Partition xylophone - Piano pas cher - Pioneer DJ - Platine vinyle Site internet DJ - Site internet Deejay - Site internet Violon - Sono Behringer - Tablature guitare - Tablatures - Ukulele - Vente sono - Yamaha RS - batterie débutant</p>
+                </div>
+
+            </div>
+        </div> 
     </footer>
 </div>
+
+<!--Oui article peu contenir section et vise versa-->
